c36e56c5968e1d19ecaa64581bb3136e8f1fb7fe
LCHIN7X <lawchinxuan@gmail.com>
2025-07-09 16:52:33 +0800

    basic

--- todo/src/App.js
+++ todo/src/App.js
@@ -5,6 +5,26 @@ function App() {
   return (
     <div className="App">
       <h1>My Todo list</h1>
+      <div className='todo-wrapper'>
+        <div className='todo-input'>
+          <div className='todo-input-item'>
+            <label>Title</label>
+            <input type="text" placeholder="What is your task?"/>
+          </div>
+          <div className='todo-input-item'>
+            <label>Description</label>
+            <input type="text" placeholder="Task description."/>
+          </div>
+          <div className='todo-input-item'>
+            <button type='button' className='primaryBtn'>Add</button>
+          </div>
+        </div>
+        <div className='btn-area'>
+          <button>ToDO</button>
+          <button>Completed</button>
+        </div>
+
+      </div>
     </div>
   );
 }
