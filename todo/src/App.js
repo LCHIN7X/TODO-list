c1fe9d6c6db68bad8f550b25c3d38adf2fd1f7a4
LCHIN7X <lawchinxuan@gmail.com>
2025-07-09 23:05:33 +0800

    add

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,6 +1,9 @@
+import React,{ useState } from 'react';
 import './App.css';
+import {AiOutlineDelete} from 'react-icons/ai'
 
 function App() {
+  const [isCompleteScreen,setIsCompleteScreen] = useState(false);
   return (
     <div className="App">
       <h1>My Todo list</h1>
@@ -20,14 +23,27 @@ function App() {
         </div>
 
         <div className='btn-area'>
-          <button>ToDO</button>
-          <button>Completed</button>
+          <button className={`secondaryBtn ${isCompleteScreen === false && 'active' }`} 
+          onClick={()=>setIsCompleteScreen(false)}>To DO</button>
+
+          <button className={`secondaryBtn ${isCompleteScreen === true && 'active' }`} 
+          onClick={()=>setIsCompleteScreen(true)}>Completed</button>
+
         </div>
 
         <div className='todo-list'>
+
+
           <div className='todo-list-item'>
-            <h3>Task 1</h3>
-            <p>Description</p>
+            <div>
+              <h3>Task 1</h3>
+              <p>Description</p>
+            </div>
+            <div>
+              <AiOutlineDelete className='icon'/>
+              <BsCheckLg className='check-icon'/>
+
+            </div>
           </div>
         </div>
 
